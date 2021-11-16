8b588c6aa7c6569706e28ae4047ee86ab6366ebd
gursimran246 <68393606+gursimran246@users.noreply.github.com>
2021-11-15 23:19:14 -0500

    Add files via upload

---
+++ Assignment2/alphabeta.py
@@ -0,0 +1,73 @@
+import math
+import complexHeuristic
+import simpleHeuristic
+
+def alphabeta(self, alpha=-math.inf, beta=math.inf, max=False):
+
+    value = math.inf
+    if max:
+        value = -math.inf
+    x = None
+    y = None
+    maxDepth = 0
+    heuristic = None
+    if self.player_turn == 'X':
+        maxDepth = self.d1
+        heuristic = self.player1Heuristic
+    else:
+        maxDepth = self.d2
+        heuristic = self.player2Heuristic
+    currentDepth = 0
+    result = self.is_end(self)
+    if result == 'X':
+        return (-1, x, y)
+    elif result == 'O':
+
+        return (1, x, y)
+    elif result == '.':
+        return (0, x, y)
+    elif currentDepth == maxDepth:
+
+
+
+
+        # heuristics - this  last level of nodes
+        currentDepth -= 1
+        if heuristic == 'e1':
+            # call simple heuristics
+            simpleHeuristic.scoreThisCell(self, x, y, self.n, self.s)
+        else:
+            # complex heuristic
+            complexHeuristic.scoreThisCell(self, x, y, self.n, self.s)
+    for i in range(0, self.n):
+        for j in range(0, self.n):
+
+            if self.current_state[i][j] == '.':
+                if max:
+                    self.current_state[i][j] = 'O'
+                    currentDepth += 1
+                    (v, _, _) = self.alphabeta(alpha, beta, max=False)
+                    if v > value:
+                        value = v
+                        x = i
+                        y = j
+                else:
+                    self.current_state[i][j] = 'X'
+                    currentDepth += 1
+                    (v, _, _) = self.alphabeta(alpha, beta, max=True)
+                    if v < value:
+                        value = v
+                        x = i
+                        y = j
+                self.current_state[i][j] = '.'
+                if max:
+                    if value >= beta:
+                        return (value, x, y)
+                    if value > alpha:
+                        alpha = value
+                else:
+                    if value <= alpha:
+                        return (value, x, y)
+                    if value < beta:
+                        beta = value
+    return (value, x, y)
